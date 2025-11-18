ea54bd43181debf32bfffb3dc3e84a5e3cbc87e9
Ángel Segarra Moros <angelsegarramoros2910@gmail.com>
2025-11-18 23:04:26 +0100

    Changes to make Vercel deployment functional.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+# Get the directory where this file is located
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, 
+            static_folder=os.path.join(basedir, 'static'),
+            static_url_path='/static',
+            template_folder=os.path.join(basedir, 'templates'))
 app.config.from_object('config')
 
 from app import views
